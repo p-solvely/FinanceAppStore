1598626b43b0604240cdab3cf9795eafc60f5c16
solvely <p.solvely@gmail.com>
2017-06-07 00:22:28 +0900

    [added] 앱 정보 값 저장하는 클래스 추가

---
+++ FinanceAppStore/FinanceAppStore/data/AppInfo.swift
@@ -0,0 +1,16 @@
+//
+//  AppInfo.swift
+//  FinanceAppStore
+//
+//  Created by sol on 2017. 6. 6..
+//  Copyright © 2017년 sol. All rights reserved.
+//
+
+import UIKit
+
+class AppInfo {
+    
+    var appId:String?
+    var title:String?
+    var iconUrl:String?
+}
